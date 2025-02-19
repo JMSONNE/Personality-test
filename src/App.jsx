08a4af4cb77f94699076207016bc79c0e103c81c
JMSONNE <jordynmsonne@gmail.com>
2025-02-18 21:49:20 -0600

    added questions and submit buttons

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,7 @@ function App() {
 
   return (
     <>
-      <div>
-        <h1>Hello</h1>
-      </div>
+      <QuizBox />
     </>
   )
 }
--- src/Components/QuizBox.jsx
+++ src/Components/QuizBox.jsx
@@ -1,11 +1,46 @@
-import React from 'react'
-import questions from '../assets/questions'
-
+import React, { useState } from 'react'
+import questions from '../assets/questions.json'
 
 export const QuizBox = () => {
-    return (
-        <div className='quiz-box'>
-            <h1 className="question"></h1>
-        </div>
-    )
+    const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+    const [isLastQuestion, setIsLastQuestion] = useState(false);
+
+    const handleNextQuestion = () => {
+        setCurrentQuestionIndex((prevIndex) => {
+            if (prevIndex < questions.length - 1) {
+                return prevIndex + 1;
+            } else {
+                setIsLastQuestion(true);
+                return prevIndex; // Return the same index if it's the last question
+            }
+        });
+    };
+
+    const currentQuestion = questions[currentQuestionIndex];
+
+    if (isLastQuestion) {
+        return (
+            <div className='quiz-box'>
+                <h1 className="question">Almost Ready...</h1>
+                <hr />
+                <button>
+                    Submit
+                </button>
+            </div>
+        );
+    } else {
+        return (
+            <div className='quiz-box'>
+                <div key={currentQuestion.id}>
+                    <h1 className="question">{currentQuestion.question}</h1>
+                    <hr />
+                    {currentQuestion.answers.map((answer, index) => (
+                        <button key={index} onClick={handleNextQuestion}>
+                            {answer.text}
+                        </button>
+                    ))}
+                </div>
+            </div>
+        );
+    }
 }
