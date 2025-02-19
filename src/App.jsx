a2c29d3d98817fa3388a8a43c88ffeb9010226da
JMSONNE <jordynmsonne@gmail.com>
2025-02-18 21:11:57 -0600

    added questions and answers

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { QuizBox } from './Components/QuizBox'
 
 function App() {
   const [count, setCount] = useState(0)
---
+++ src/Components/QuizBox.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import questions from '../assets/questions'
+
+
+export const QuizBox = () => {
+    return (
+        <div className='quiz-box'>
+            <h1 className="question"></h1>
+        </div>
+    )
+}
