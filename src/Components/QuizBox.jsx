0e1ab6149dd57185004be3f8ad7e4aef757ba536
JMSONNE <jordynmsonne@gmail.com>
2025-02-21 18:49:07 -0600

    added restart test button

--- src/Components/QuizBox.jsx
+++ src/Components/QuizBox.jsx
@@ -97,21 +97,23 @@ export const QuizBox = () => {
                         <h1 className='title'>You are:</h1>
                         <hr />
                         <div className="result-box">
-                            <p className='value code'>{personalityType}</p>
+                            <p className='value code' style={{ marginBottom: '1rem' }}>{personalityType}</p>
                             {personalityTraits && (
                                 <>
                                     <h1 className="value">Name:</h1>
-                                    <p className='value'>{personalityTraits.type}</p>
+                                    <p className='value' style={{ marginBottom: '1rem' }}>{personalityTraits.type}</p>
                                     <h1 className="value">Key traits:</h1>
-                                    <p className='value'>{personalityTraits.traits}</p>
+                                    <p className='value' style={{ marginBottom: '1rem' }}>{personalityTraits.traits}</p>
                                     <h1 className="value">Description:</h1>
                                     <p className='value'>{personalityTraits.description}</p>
                                 </>
                             )}
+
+                            <div className="button" onClick={() => window.location.reload()}>Restart</div>
                         </div>
                     </div>
                 </div>
-            </div>
+            </div >
         );
     }
 
